debfe923bbbf96cde5579b5505a90cc84c539bcc
jatinvds <jatin.v@datasturdy.com>
2023-05-29 17:41:58 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Component1 from './component1'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Youtube</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component1.jsx
@@ -0,0 +1,11 @@
+function Component1 ()
+{
+    return(
+        <div className="comp">
+            <h1>Component one</h1>
+            <h1>Component one</h1>
+        </div>
+        
+    )
+}
+export default Component1;
